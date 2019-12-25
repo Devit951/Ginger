35c1d65b53a3d0dab178991757365b9dd1f10d65
Devit951 <devit951@gmail.com>
2019-12-25 12:15:06 +0500

    Formatted code

--- app/src/main/java/devit951/github/ginger/MainActivity.kt
+++ app/src/main/java/devit951/github/ginger/MainActivity.kt
@@ -1,8 +1,8 @@
 package devit951.github.ginger
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import devit951.github.ginger_bottombar.GingerBottomBarView
 import devit951.github.ginger_bottombar.GingerItem
 import devit951.github.ginger_ui.*
@@ -19,28 +19,37 @@ class MainActivity : AppCompatActivity() {
             aVerticalLayout {
                 aFrameLayout {
                     id = ID_MAIN_ROOT_FRAGMENT
-                    linearParams(width = matchParent, height = 0){
+                    linearParams(width = matchParent, height = 0) {
                         weight = 1f
                     }
                 }
                 addView(GingerBottomBarView(context).apply {
                     overlayView.setBackgroundColor(color(R.color.colorPrimary))
                     gingerItems = listOf(
-                        GingerItem(android.R.drawable.ic_delete, android.R.string.cancel, onClickListener = {
-                            autoHideShowFragment(savedFragment(ID_MAIN_ROOT_FRAGMENT, "1"){
-                                EmptyFragment(Color.GREEN)
+                        GingerItem(
+                            android.R.drawable.ic_delete,
+                            android.R.string.cancel,
+                            onClickListener = {
+                                autoHideShowFragment(savedFragment(ID_MAIN_ROOT_FRAGMENT, "1") {
+                                    EmptyFragment(Color.GREEN)
+                                })
+                            }),
+                        GingerItem(
+                            android.R.drawable.ic_dialog_alert,
+                            android.R.string.copyUrl,
+                            onClickListener = {
+                                autoHideShowFragment(savedFragment(ID_MAIN_ROOT_FRAGMENT, "2") {
+                                    EmptyFragment(Color.RED)
+                                })
+                            }),
+                        GingerItem(
+                            android.R.drawable.ic_input_add,
+                            android.R.string.cut,
+                            onClickListener = {
+                                autoHideShowFragment(savedFragment(ID_MAIN_ROOT_FRAGMENT, "3") {
+                                    EmptyFragment(Color.BLUE)
+                                })
                             })
-                        }),
-                        GingerItem(android.R.drawable.ic_dialog_alert, android.R.string.copyUrl, onClickListener = {
-                            autoHideShowFragment(savedFragment(ID_MAIN_ROOT_FRAGMENT, "2"){
-                                EmptyFragment(Color.RED)
-                            })
-                        }),
-                        GingerItem(android.R.drawable.ic_input_add, android.R.string.cut, onClickListener = {
-                            autoHideShowFragment(savedFragment(ID_MAIN_ROOT_FRAGMENT, "3"){
-                                EmptyFragment(Color.BLUE)
-                            })
-                        })
                     )
                     linearParams(width = matchParent)
                 })
@@ -48,7 +57,7 @@ class MainActivity : AppCompatActivity() {
             frameParams(width = matchParent, height = matchParent)
         })
         if (savedInstanceState == null) {
-            autoHideShowFragment(savedFragment(ID_MAIN_ROOT_FRAGMENT, "1"){
+            autoHideShowFragment(savedFragment(ID_MAIN_ROOT_FRAGMENT, "1") {
                 EmptyFragment(Color.GREEN)
             })
         }
