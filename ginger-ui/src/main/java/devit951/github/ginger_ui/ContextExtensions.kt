532beec89f8d6bd7ec441ab08e010fdb56f799c2
Devit951 <devit951@gmail.com>
2019-12-26 17:10:02 +0500

    Added dpToPx

--- ginger-ui/src/main/java/devit951/github/ginger_ui/ContextExtensions.kt
+++ ginger-ui/src/main/java/devit951/github/ginger_ui/ContextExtensions.kt
@@ -1,9 +1,12 @@
 package devit951.github.ginger_ui
 
 import android.content.Context
+import android.content.res.Resources
 import android.view.View
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
 
 fun Context.color(@ColorRes colorRes: Int) = ContextCompat.getColor(this, colorRes)
 fun View.color(@ColorRes colorRes: Int) = context.color(colorRes)
+
+fun dpToPx(dp: Int) = dp * Resources.getSystem().displayMetrics.density.toInt()
