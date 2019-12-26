9c36447a669ed9ed1f90cfeb06fb9414660339bc
Devit951 <devit951@gmail.com>
2019-12-26 17:09:21 +0500

    Added button{}

--- ginger-ui/src/main/java/devit951/github/ginger_ui/Widgets.kt
+++ ginger-ui/src/main/java/devit951/github/ginger_ui/Widgets.kt
@@ -1,6 +1,7 @@
 package devit951.github.ginger_ui
 
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -10,6 +11,11 @@ fun <T: ViewGroup> T.textView(init: TextView.() -> Unit) = TextView(context).app
 }
 
 fun <T: ViewGroup> T.imageView(init: ImageView.() -> Unit) = ImageView(context).apply{
+    init(this)
+    addView(this)
+}
+
+fun <T: ViewGroup> T.button(init: Button.() -> Unit) = Button(context).apply{
     init(this)
     addView(this)
 }
