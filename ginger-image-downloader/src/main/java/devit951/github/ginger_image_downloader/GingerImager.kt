d874e5bc7145c3f52c855067a5455edf6db9d0ca
Devit951 <devit951@gmail.com>
2019-12-28 15:58:55 +0500

    Added ginger-image-downloader for laod photos from internet

---
+++ ginger-image-downloader/src/main/java/devit951/github/ginger_image_downloader/GingerImager.kt
@@ -0,0 +1,16 @@
+package devit951.github.ginger_image_downloader
+
+import android.graphics.BitmapFactory
+import android.widget.ImageView
+import java.net.URL
+import java.util.concurrent.Executors
+
+internal val threadPool = Executors.newFixedThreadPool(4)
+
+
+fun ImageView.load(url: String){
+    threadPool.submit {
+        val decodedStream = BitmapFactory.decodeStream(URL(url).openStream())
+        setImageBitmap(decodedStream)
+    }
+}
