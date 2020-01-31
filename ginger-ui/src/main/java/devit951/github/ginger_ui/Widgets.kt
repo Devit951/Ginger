91a7a3897d5b25aea7254f02207094b293c4209b
Devit951 <devit951@gmail.com>
2020-01-31 17:21:30 +0500

    Added recyclerView

--- ginger-ui/src/main/java/devit951/github/ginger_ui/Widgets.kt
+++ ginger-ui/src/main/java/devit951/github/ginger_ui/Widgets.kt
@@ -4,6 +4,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 fun <T: ViewGroup> T.textView(init: TextView.() -> Unit) = TextView(context).apply{
     init(this)
@@ -18,4 +19,9 @@ fun <T: ViewGroup> T.imageView(init: ImageView.() -> Unit) = ImageView(context).
 fun <T: ViewGroup> T.button(init: Button.() -> Unit) = Button(context).apply{
     init(this)
     addView(this)
+}
+
+fun <T: ViewGroup> T.recyclerView(init: RecyclerView.() -> Unit) = RecyclerView(context).also{
+    init(it)
+    addView(it)
 }
